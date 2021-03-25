51812bc6d82633ecc27a3c37f013a3e58266d9d7
AlexandreCare <70692837+AlexandreCare@users.noreply.github.com>
2021-03-25 15:39:40 +0100

    Add files via upload

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+ main()
+{
+ int a, b, c;
+
+ printf("Calcul des solutions reelles d'une équation du second \n");
+ printf("degre de la forme   ax^2 + bx + c = 0 \n\n");
+
+ saisie(a,b,c);
+
+ calcul(a,b,c);
+}
+
+  return 0;
+ }
